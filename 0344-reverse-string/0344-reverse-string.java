626c612bd5e774675212c774e9e373ab57f4c692
RACHIT PANDEY <rachitpandey0603@gmail.com>
2026-09-11 12:10:10 +0530

    Time: 0 ms (100%), Space: 48.4 MB (21.36%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public  static void swap(char[] s ,int a , int b){
+        char temp ;
+        temp = s[a] ;
+        s[a]=s[b];
+        s[b]=temp;
+    }
+    public void reverseString(char[] s) {
+        int i =0 ;
+        int j = s.length-1;
+        while(i <= j ){
+            swap(s,i,j);
+        i++;
+        j--;
+        }
+
+    }
+}
